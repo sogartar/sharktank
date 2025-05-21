f95a001727c198ef64efca7d0ba31be732ea9f73
Boian Petkantchin <boian.petkantchin@amd.com>
2025-05-21 01:41:08 +0000

    [sharktank] Add missing argument device affinity for tensor-parallel Resnet block test
    The test needs to be xfailed due to compiler error
    https://github.com/iree-org/iree/issues/21049

--- sharktank/tests/models/punet/sharded_resnet_block_with_iree_test.py
+++ sharktank/tests/models/punet/sharded_resnet_block_with_iree_test.py
@@ -7,8 +7,7 @@
 import gc
 import iree.compiler
 import iree.runtime
-import pytest
-import sys
+import re
 import tempfile
 import torch
 
@@ -26,6 +25,7 @@ from sharktank.utils.iree import (
     run_iree_module_function,
     with_iree_device_context,
 )
+from sharktank.utils.testing import xfail
 from typing import List
 
 
@@ -100,7 +100,7 @@ def run_test_toy_size_sharded_resnet_block_with_iree(artifacts_dir: Path):
                 vm_context=iree_vm_context,
                 args=iree_args,
                 device=iree_devices[0],
-                function_name=f"main",
+                function_name="forward",
             )
         )
         return [
@@ -114,18 +114,14 @@ def run_test_toy_size_sharded_resnet_block_with_iree(artifacts_dir: Path):
     torch.testing.assert_close(actual_outputs, expected_results, rtol=0, atol=5e-5)
 
 
-@pytest.mark.xfail(
-    raises=iree.compiler.tools.binaries.CompilerToolError,
-    reason="https://github.com/nod-ai/shark-ai/issues/1576",
+@xfail(
+    raises=iree.compiler.CompilerToolError,
+    reason=(
+        "The compiler crashes."
+        " See IREE issue https://github.com/iree-org/iree/issues/21049."
+    ),
     strict=True,
-)
-@pytest.mark.xfail(
-    torch.__version__ >= (2, 5),
-    reason="https://github.com/nod-ai/shark-ai/issues/683",
-    strict=True,
-)
-@pytest.mark.skipif(
-    sys.platform == "win32", reason="https://github.com/nod-ai/shark-ai/issues/698"
+    match=re.escape(r"Error code: -11"),
 )
 def test_toy_size_sharded_resnet_block_with_iree():
     """Test sharding, exportation and execution with IREE local-task of a Resnet block.
